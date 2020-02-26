c575e385e05038bbc24b950890e9754885e50972
Edward Schults <edward.schults@gmail.com>
2020-02-26 19:23:29 +0100

    optimize seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,83 +1,63 @@
 require 'csv'
 
-puts "parsing products..."
-products = []
-CSV.foreach('db/brazilian-ecommerce/olist_products_dataset.csv', headers: true) do |row|
-  print "."
-  products << Product.new(row.to_hash)
+def import_items(elements, mapping = true)
+  puts "      Importing #{elements.size} #{elements.first.class.name.downcase.pluralize}..."
+  elements.first.class.import elements
+  puts "      Done!"
+  elements.first.class.pluck(:csv_id, :id).to_h if mapping
 end
 
-puts "\nimporting #{products.size} products..."
-Product.import products
-puts "done!"
-
-puts "parsing sellers..."
-sellers = []
-CSV.foreach('db/brazilian-ecommerce/olist_sellers_dataset.csv', headers: true) do |row|
-  print "."
-  sellers << Seller.new(row.to_hash)
+def parse_items(klass, step)
+  pluralized_name = klass.name.downcase.pluralize
+  puts "[#{step}/6] Parsing #{pluralized_name}..."
+  elements = []
+  CSV.foreach("db/brazilian-ecommerce/olist_#{pluralized_name}_dataset.csv", headers: true) do |row|
+    elements << klass.new(row.to_hash)
+  end
+  import_items(elements)
 end
 
-puts "\nimporting #{sellers.size} sellers..."
-Seller.import sellers
-puts "done!"
+product_ids_mapping  = parse_items(Product,  1)
+seller_ids_mapping   = parse_items(Seller,   2)
+customer_ids_mapping = parse_items(Customer, 3)
+GC.start
 
-puts "parsing customers..."
-customers = []
-CSV.foreach('db/brazilian-ecommerce/olist_customers_dataset.csv', headers: true) do |row|
-  print "."
-  customers << Customer.new(row.to_hash)
-end
-
-puts "\nimporting #{customers.size} customers..."
-Customer.import customers
-puts "done!"
-
-puts "parsing orders..."
+puts "[4/6] Parsing orders..."
 offset = (Date.today - Date.new(2017, 3, 1)).to_i
 orders = []
 CSV.foreach('db/brazilian-ecommerce/olist_orders_dataset.csv', headers: true) do |row|
   next if row["purchased_at"].nil? || row["purchased_at"] < "2017-03-01" || row["purchased_at"] > "2018-03-31"
 
-  print "."
   order = Order.new(row.to_hash)
-  order.customer = Customer.find_by(csv_id: row["customer_csv_id"])
+  order.customer_id = customer_ids_mapping[row["customer_csv_id"]]
   order.purchased_at = order.purchased_at + offset.days
   orders << order
 end
-
-puts "\nimporting #{orders.size} orders..."
-Order.import orders
-puts "done!"
+order_ids_mapping = import_items(orders)
+GC.start
 
 
-puts "parsing order items..."
+puts "[5/6] Parsing order items..."
 order_items = []
 CSV.foreach('db/brazilian-ecommerce/olist_order_items_dataset.csv', headers: true) do |row|
   order_item = OrderItem.new(row.to_hash)
-  order_item.order = Order.find_by(csv_id: row["order_csv_id"])
+  order_item.order_id = order_ids_mapping[row["order_csv_id"]]
   next if order_item.order.nil?
 
-  print "."
-  order_item.product = Product.find_by(csv_id: row["product_csv_id"])
-  order_item.seller = Seller.find_by(csv_id: row["seller_csv_id"])
+  order_item.product_id = product_ids_mapping[row["product_csv_id"]]
+  order_item.seller_id = seller_ids_mapping[row["seller_csv_id"]]
   order_items << order_item
 end
+import_items(order_items, false)
+GC.start
 
-puts "\nimporting #{order_items.size} order_items..."
-OrderItem.import order_items
-puts "done!"
-
-puts "parsing payments..."
+puts "[6/6] Parsing payments..."
 order_payments = []
 CSV.foreach('db/brazilian-ecommerce/olist_order_payments_dataset.csv', headers: true) do |row|
-  print "."
   order_payment = OrderPayment.new(row.to_hash)
-  order_payment.order = Order.find_by(csv_id: row["order_csv_id"])
+  order_payment.order_id = order_ids_mapping[row["order_csv_id"]]
   order_payments << order_payment
 end
+import_items(order_payments, false)
 
-puts "\nimporting #{order_payments.size} order_payments..."
-OrderPayment.import order_payments
-puts "done!"
-
+puts "Completed global import"
